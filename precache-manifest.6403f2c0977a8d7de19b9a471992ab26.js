d10d6cbd9656e6ee0295a52d2dadad038a4f7521
lime7 <senseless.v.7@gmail.com>
2021-06-12 10:07:32 +0300

    deploy

---
+++ precache-manifest.6403f2c0977a8d7de19b9a471992ab26.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/todo/fonts/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/todo/fonts/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/todo/fonts/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/todo/fonts/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/todo/fonts/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/todo/fonts/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/todo/fonts/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/todo/fonts/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/todo/fonts/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/todo/fonts/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/todo/fonts/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/todo/fonts/fa-solid-900.e8a427e1.woff2"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/todo/img/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/todo/img/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/todo/img/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "73decb2e902a2c5cd14ae11bd654b738",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f3cb0ddceda14e90d679",
+    "url": "/todo/js/app.8250943b.js"
+  },
+  {
+    "revision": "ce9797d9dc8e8049dd81",
+    "url": "/todo/js/chunk-vendors.f4daeef6.js"
+  },
+  {
+    "revision": "f58f77eeacbb6656cc3e",
+    "url": "/todo/js/todos.876e083c.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "/todo/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/todo/robots.txt"
+  }
+]);
